1de8fc22e9cd670fbb07a5a94828083142d8e427
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2024-07-21 23:03:40 -0300

    Entendendo o código

--- Ex006/rotas/rotas.js
+++ Ex006/rotas/rotas.js
@@ -16,6 +16,10 @@ rotas.get('/',(req,res)=>{
     res.json({ola:'Seja bem-vindo'})
 })
 
+
+// Essa linha de código abaixo significa que oque tiver após a 
+/// sera armazenado na const curso ou seja em cursoi esta verificando se o curso armazenado na const curso é igual a o que 
+//está escrito na url se for true ele escreve o curso e as informações em um objeto json.
 rotas.get('/:cursoid',(req,res)=>{
     const curso = req.params.cursoid
     const cursoi = cursos_info.find(i=>i.Curso == curso)
